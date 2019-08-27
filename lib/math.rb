61240d64acde9c445933aa856f2feb693222e1a2
Laura Berge <lauraeberge@gmail.com>
2019-08-27 23:00:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  p num1 - num2
+  result = num1 - num2
+  p result
 end
 
 def division(num1, num2)
-  p num1 / num2
+  result = num1 / num2
+  p result
 end
 
 def multiplication(num1, num2)
-  p num1 * num2
+  result = num1 * num2
+  p result
 end
 
 def modulo(num1, num2)
-  p num1 % num2
+  result = num1 % num2
+  p result
 end
 
 def square_root(num)
-   
+   result = Math.sqrt(num)
+   p result
 end
